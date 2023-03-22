1f3a83aa7229d5fcfe8cc209515a606a5e435076
NevenGerencir <gerencirn@gmail.com>
2023-03-22 15:01:08 +0100

    stereotype annotation

---
+++ sq-ch2-ex5/src/main/java/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import components.Parrot;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(config.ProjectConfig.class);
+        var p = context.getBean(Parrot.class);
+        p.setName("Kiki");
+        System.out.println(p.getName());
+    }
+}
