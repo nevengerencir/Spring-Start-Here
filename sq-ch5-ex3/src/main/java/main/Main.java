2fcae9c159652c5c22aef9f83372649c7e691368
NevenGerencir <gerencirn@gmail.com>
2023-03-26 08:41:58 +0200

    prototype scope of a bean - Spring creates a different instance of a bean everytime a bean is called

--- sq-ch5-ex3/src/main/java/main/Main.java
+++ sq-ch5-ex3/src/main/java/main/Main.java
@@ -5,7 +5,9 @@ import services.CommentService;
 
 public class Main {
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        var commentService = context.getBean(CommentService.class);
+        var context1 = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var context2 = new AnnotationConfigApplicationContext(ProjectConfig.class);
+    Boolean b = context2 == context1;
+    System.out.println(b);
     }
 }
--- sq-ch5-ex3/src/main/java/services/CommentService.java
+++ sq-ch5-ex3/src/main/java/services/CommentService.java
@@ -1,8 +1,10 @@
 package services;
 
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-@Lazy
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 @Component
 public class CommentService {
     public CommentService(){
