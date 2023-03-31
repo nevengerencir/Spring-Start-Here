d57c68e796a5eea609c56d8fa9b3cb3e9ba57089
NevenGerencir <gerencirn@gmail.com>
2023-03-31 17:27:11 +0200

    request query parameters

--- sq-ch7-ex1/src/main/java/main/sqch7ex1/controllers/MainController.java
+++ sq-ch7-ex1/src/main/java/main/sqch7ex1/controllers/MainController.java
@@ -3,14 +3,19 @@ package main.sqch7ex1.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
 public class MainController {
     @RequestMapping("/home")
-    public String home(Model page) {
-        page.addAttribute("username", "Katy");
-        page.addAttribute("color", "red");
+    public String home(
+            @RequestParam(required = false) String name,
+            @RequestParam String color,
+    Model page) {
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
         return "test.html";
     }
 }
