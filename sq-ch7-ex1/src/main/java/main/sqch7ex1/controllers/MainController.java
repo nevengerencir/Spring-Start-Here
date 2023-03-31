e234446f57d94f1ee213be1f728831f51203d46a
NevenGerencir <gerencirn@gmail.com>
2023-03-31 17:33:20 +0200

    path variable example

--- sq-ch7-ex1/src/main/java/main/sqch7ex1/controllers/MainController.java
+++ sq-ch7-ex1/src/main/java/main/sqch7ex1/controllers/MainController.java
@@ -2,6 +2,7 @@ package main.sqch7ex1.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,10 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-    @RequestMapping("/home")
+    @RequestMapping("/home/{color}")
     public String home(
             @RequestParam(required = false) String name,
-            @RequestParam String color,
+            @PathVariable String color,
     Model page) {
         page.addAttribute("username", name);
         page.addAttribute("color", color);
