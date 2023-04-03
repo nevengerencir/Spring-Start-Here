c594b6e7cbc08c073b219b00430a0adc46c0228c
NevenGerencir <gerencirn@gmail.com>
2023-04-03 10:18:01 +0200

    ResponseEntity

--- sq-ch10-ex1/src/main/java/sqch10ex1/main/controllers/BookController.java
+++ sq-ch10-ex1/src/main/java/sqch10ex1/main/controllers/BookController.java
@@ -1,16 +1,22 @@
 package sqch10ex1.main.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sqch10ex1.main.model.Book;
 
+import java.util.List;
+
 
 @RestController
 public class BookController {
     @GetMapping("/book")
-    public Book lotr (@RequestParam String title, @RequestParam String author){
+    public ResponseEntity<Book> books (@RequestParam String title, @RequestParam String author){
         Book book = new Book(author,title);
-        return book;
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .header("material","leather")
+                .body(book);
     }
 }
